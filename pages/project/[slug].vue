094c096feeb3ee8b1dfc97b9f172e4dca9408110
angel <angel.shanti457@gmail.com>
2022-09-22 10:14:34 +0700

    initial commit

---
+++ pages/project/[slug].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute();
+const story = await useStoryblok(`project/${route.params.slug}`,{
+    version: "draft",
+});
+</script>
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+</template>
